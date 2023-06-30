5eb4f967b33e8ce0ed5a8501cf6f9abb0ae39c62
Andres Soria <andres.zov@gmail.com>
2023-06-30 17:44:39 -0600

    Set up main routes with dummy content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/new' element={<h1>New</h1>} />
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
+  );
 }
 
 export default App;
