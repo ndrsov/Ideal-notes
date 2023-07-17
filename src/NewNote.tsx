a3ba0848fa010e37e25ff5ef3f31281d11b48b53
Andres Soria <andres.zov@gmail.com>
2023-07-17 17:03:18 -0600

    Set NewNoteProps tye onSubmit

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,10 +1,12 @@
+import { NoteData } from './App';
 import { NoteForm } from './NoteForm';
+type NewNoteProps = { onSubmit: (data: NoteData) => void };
 
-export function NewNote() {
+export function NewNote({ onSubmit }: NewNoteProps) {
   return (
     <>
       <h1 className='mb-4'>New Note</h1>
-      <NoteForm />
+      <NoteForm onSubmit={onSubmit} />
     </>
   );
 }
