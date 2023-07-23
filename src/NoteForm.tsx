b6aed9d65732017312be417c2aa9f4d1ef69c869
Andres Soria <andres.zov@gmail.com>
2023-07-23 14:08:51 -0600

    Add useNavigate hook to return to previous page after saving

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react';
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import CreatableSelect from 'react-select/creatable';
 import { NoteData, Tag } from './App';
 import { v4 as uuidv4 } from 'uuid';
@@ -15,6 +15,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }): NoteFormProps {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const navigate = useNavigate();
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
@@ -24,6 +25,8 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }): NoteFormProps {
       markdown: markdownRef.current!.value,
       tags: selectedTags,
     });
+
+    navigate('...');
   }
 
   return (
