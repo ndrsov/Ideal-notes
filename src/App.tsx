7686ec82e68616aa3b33e86b63615a8110e19e60
Andres Soria <andres.zov@gmail.com>
2023-07-25 13:27:11 -0600

    Import NoteLIst component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewNote } from './NewNote';
 import { useLocalStorage } from './useLocalStorage';
 import { useMemo } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { NoteList } from './NoteList';
 
 export type Note = {
   id: string;
