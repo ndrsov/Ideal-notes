bd5ccb3a56b915952668a4c378d7e64c36c599be
Andres Soria <andres.zov@gmail.com>
2023-07-27 10:53:26 -0600

    Add Title and tags filter on NoteLIst

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,7 +1,15 @@
-import { Button, Col, Row, Stack } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
+import ReactSelect from 'react-select';
+import { Tag } from './App';
 import { Link } from 'react-router-dom';
 
-export function NoteList() {
+type NoteListProps = {
+  availableTags: Tag[];
+};
+
+export function NoteList({ availableTags }: NoteListProps) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   return (
     <>
       <Row>
@@ -17,6 +25,37 @@ export function NoteList() {
           </Stack>
         </Col>
       </Row>
+      <Form>
+        <Row className='mb-4'>
+          <Col>
+            <Form.Group controlId='title'>
+              <Form.Label>Title</Form.Label>
+              <Form.Control type='text' />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId='tags'>
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tags) => {
+                      return { label: tags.label, id: tags.value };
+                    })
+                  );
+                }}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
     </>
   );
 }
