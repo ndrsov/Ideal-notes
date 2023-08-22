de095e2ad1ce1fd3515cc50293b50fb9ea54f8b7
Andres Soria <andres.zov@gmail.com>
2023-08-22 14:12:48 -0600

    Pass onDeleteNote function to Note element

--- src/App.tsx
+++ src/App.tsx
@@ -97,7 +97,7 @@ function App() {
           }
         />
         <Route path='/:id' element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route
             path='edit'
             element={
