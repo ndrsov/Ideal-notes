277512d5fc05a5206c3bc2ead8ce957a7ded8c01
Andres Soria <andres.zov@gmail.com>
2023-09-04 14:33:37 -0600

    Pass props to EditTagsModal

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -105,7 +105,7 @@ export function NoteList({ availableTags, notes }: NoteListProps) {
           </Col>
         ))}
       </Row>
-      <EditTagsModal />
+      <EditTagsModal show={} handleClose={} availableTags={availableTags} />
     </>
   );
 }
